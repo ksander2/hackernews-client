43721f3ce056f0bb4905a49cf48bf173cec4e790
Ksander2 <asobolev@aspirity.com>
2020-11-04 21:41:47 +0700

    add hash router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 import { Provider } from 'react-redux';
